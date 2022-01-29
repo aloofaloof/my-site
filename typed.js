c0394a5a616377d5d6609680142283b5ffa5513e
aloofaloof <98646584+aloofaloof@users.noreply.github.com>
2022-01-29 23:39:11 +0400

    Update typed.js

--- typed.js
+++ typed.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
 new TypeIt("#type", {
     speed: 125,
-    loop: false,
+    loop: true,
     nextStringDelay: 750,
     deleteSpeed: 150,
     cursor: true,
