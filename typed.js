2bce68d32efb284765e224f287e86209340da83a
aloofaloof <98646584+aloofaloof@users.noreply.github.com>
2022-02-01 13:10:31 +0400

    Add files via upload

--- typed.js
+++ typed.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
 new TypeIt("#type", {
     speed: 125,
-    loop: true,
+    loop: false,
     nextStringDelay: 750,
     deleteSpeed: 150,
     cursor: true,
